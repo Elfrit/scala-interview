b1e9028752cdb52d5fccb6983d486e7456e36fc4
Mekhanef Sami <43059640+Elfrit@users.noreply.github.com>
2020-03-06 14:24:20 +0100

    Update BasicActor.scala

--- app/akka/BasicActor.scala
+++ app/akka/BasicActor.scala
@@ -11,14 +11,21 @@ import akka.actor.{ Actor, ActorSystem, Props }
 class BasicActor {}
 
 object FireActor {
+   
+  val log = Logging(context.system, this)
 
+  def receive = {
+    case "test" => log.info("Hello there.")
+    case _      => log.info("What ?")
+  }
+  
   /**
     * Create an instance of BasicActor
     *
     * Make it print "Hello there." and "What?"
     */
   def fireActor(): Unit = {
-    //    val system = ActorSystem("Actor System")
-    //    val basic_actor = system.actorOf(Props[BasicActor], name = "basic actor")
+        val system = ActorSystem("Actor System")
+        val basic_actor = system.actorOf(Props[BasicActor], name = "basic actor")
   }
 }
