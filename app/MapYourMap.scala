03843dfaa2cd4ad0c15237e259650332bf1b7822
Mekhanef Sami <43059640+Elfrit@users.noreply.github.com>
2020-03-06 14:03:17 +0100

    Update MapYourMap.scala

--- app/MapYourMap.scala
+++ app/MapYourMap.scala
@@ -10,5 +10,16 @@ object MapYourMap {
   val devNames = Map("dev1" -> "Pierre", "dev2" -> "Remy", "dev3" -> "Noe", "dev4" -> "Alexandre")
   val devDepartments = Map("dev1" -> "analytics", "dev2" -> "frontend", "dev3" -> "api", "dev4" -> "frontend")
 
-  val namesInDepartments:Map[String, List[String]] = ???
+  val namesInDepartments:scala.collection.mutable.Map[String, List[String]] ={
+  devNames.foreach(i =>
+            devDepartments.foreach(j =>
+              if (namesInDepartments.contains(j._2)==false && (j._1.equals(i._1))){
+                namesInDepartments = namesInDepartments + (j._2->List(i._2))
+              }
+              else if(j._1.equals(i._1)){
+                 namesInDepartments(j._2) = namesInDepartments(j._2):::List(i._2)
+              }
+            )
+  )
+ }
 }
